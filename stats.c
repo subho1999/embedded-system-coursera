ea4732994558819128cece46bd04a2b5bf37ddc8
subho1999 <ghosh.subho.1999@gmail.com>
2019-10-12 00:58:34 +0530

    Quick Fix of Ascending to Descending Order

--- stats.c
+++ stats.c
@@ -71,7 +71,7 @@ void sort_array(unsigned char *arr, unsigned int size){
   unsigned int i, j, temp;
   for(i = 0; i < size; ++i){
     for(j = 0; j < size-1-i; ++j){
-      if(arr[j] > arr[j+1]){
+      if(arr[j] < arr[j+1]){
         temp = arr[j];
         arr[j] = arr[j+1];
         arr[j+1] = temp;
